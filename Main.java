822cae4a7d327d988beff8caae799d4e56bf2403
miguel <miguel@Miguels-MacBook-Air.local>
2022-09-21 23:32:45 -0600

    Push medianoche entrega

--- Main.java
+++ Main.java
@@ -7,8 +7,53 @@ public class Main {
         try {
             parser p = new parser(new Lexer(new FileReader("./input.txt")));
             p.parse();
+
+            if(parser.num_erroresSintactico == 0){
+                Nodo raiz = parser.padre;
+                //
+                System.out.println("Se genero el AST.");
+                Graficar(recorrido(raiz));
+            } else {
+                System.out.println("No se genero el AST.");
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
     }
+
+    private static String recorrido(Nodo raiz) {
+        String cuerpo = "";
+      for (Nodo child : raiz.hijos) {
+          // System.out.println("hola");
+            if (!(child.getEtiqueta().equals("VACIO"))) {
+              cuerpo += "\"" + raiz.getID() + ". " + raiz.getEtiqueta() + " = " + raiz.getValor() +
+                  "\"->\""+ child.getID() +". " + child.getEtiqueta()  + " = " + child.getValor() + "\""  + "\n";
+              cuerpo += recorrido(child);
+            }
+      }
+      return cuerpo;
+  }
+
+    public static void Graficar(String cadena) {
+        FileWriter fw = null;
+        PrintWriter pw = null;
+        String nombreArchivo = "AST.dot";
+        try {
+            fw  = new FileWriter(nombreArchivo);
+            pw = new PrintWriter(fw);
+            pw.println("digraph G {");
+            pw.println(cadena);
+            pw.println("\n}");
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            String cmd = "dot -Tpng AST.dot -o imagenAST.png";
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+    }
+
 }
--- sym.java
+++ sym.java
@@ -7,7 +7,7 @@
 public class sym {
   /* terminals */
   public static final int ELSE = 3;
-  public static final int CONSTSTRING = 22;
+  public static final int CONSTSTRING = 21;
   public static final int INT = 12;
   public static final int CHAR = 13;
   public static final int WHILE = 4;
@@ -16,30 +16,30 @@ public class sym {
   public static final int DOBLEPUNTOS = 32;
   public static final int IF = 2;
   public static final int PRINTF = 10;
-  public static final int ID = 24;
-  public static final int APUNTADOR = 14;
-  public static final int OPMULT = 16;
+  public static final int ID = 23;
+  public static final int APUNTADOR = 25;
+  public static final int OPMULT = 15;
   public static final int COMMA = 26;
-  public static final int ASIGNACION = 18;
+  public static final int ASIGNACION = 17;
   public static final int EOF = 0;
-  public static final int APUNTADORVAR = 25;
-  public static final int AUTOINCREMENTOS = 17;
+  public static final int APUNTADORVAR = 24;
+  public static final int AUTOINCREMENTOS = 16;
   public static final int MAIN = 6;
   public static final int BREAK = 7;
-  public static final int CONSTCHAR = 23;
+  public static final int CONSTCHAR = 22;
   public static final int error = 1;
   public static final int SINTERROGACION = 33;
   public static final int PUNTOCOMA = 27;
-  public static final int INTEGER = 21;
+  public static final int INTEGER = 20;
   public static final int RPAR = 29;
-  public static final int OPSUM = 15;
+  public static final int OPSUM = 14;
   public static final int FOR = 5;
-  public static final int OPREL = 19;
+  public static final int OPREL = 18;
   public static final int VOID = 9;
   public static final int DERLLAVE = 31;
   public static final int IZQLLAVE = 30;
   public static final int LPAR = 28;
-  public static final int OPLOGIC = 20;
+  public static final int OPLOGIC = 19;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
@@ -55,7 +55,6 @@ public class sym {
   "SCANF",
   "INT",
   "CHAR",
-  "APUNTADOR",
   "OPSUM",
   "OPMULT",
   "AUTOINCREMENTOS",
@@ -67,6 +66,7 @@ public class sym {
   "CONSTCHAR",
   "ID",
   "APUNTADORVAR",
+  "APUNTADOR",
   "COMMA",
   "PUNTOCOMA",
   "LPAR",
